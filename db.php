ec8dae67210d77332838b153896532345bbfb915
Ranncioo <cioran694@gmail.com>
2026-05-08 17:14:41 +0700

    Update konfigurasi database ke Railway MySQL

--- db.php
+++ db.php
@@ -1,17 +1,15 @@
 <?php
-
-$servername = "146.190.88.116";
-$username = "root";
-$password = "informatika";
-$db = "khanstore";
+$servername = getenv('MYSQLHOST') ?: 'gondola.proxy.rlwy.net';
+$port       = getenv('MYSQLPORT') ?: '39875';
+$username   = getenv('MYSQLUSER') ?: 'root';
+$password   = getenv('MYSQLPASSWORD') ?: 'XnkOhlzMltPtsixjxNdYbxvbkTDPFxvd';
+$db         = getenv('MYSQL_DATABASE') ?: 'railway';
 
 // Create connection
-$con = mysqli_connect($servername, $username, $password,$db);
+$con = mysqli_connect($servername, $username, $password, $db, $port);
 
 // Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-
 ?>
